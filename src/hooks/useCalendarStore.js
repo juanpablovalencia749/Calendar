721755a8030ba688063d11509e75a3abc7428c40
Juan Pablo <juanpablovalencia749@gmail.com>
2023-11-28 18:07:11 -0500

    Frontend 1.0

---
+++ src/hooks/useCalendarStore.js
@@ -0,0 +1,76 @@
+import { useDispatch, useSelector } from "react-redux"
+import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from "../store";
+import calendarApi from "../api/calendarApi";
+import { convertEventsToDateEvents } from "./convertEventsToDateEvents";
+import Swal from "sweetalert2";
+
+export const useCalendarStore = () => {
+
+  const dispatch = useDispatch();
+  const { events, activeEvent } = useSelector( state =>state.calendar );
+  const { user } = useSelector( state => state.auth );
+
+  const setActiveEvent = ( calendarEvent ) => {
+    dispatch( onSetActiveEvent( calendarEvent ) )
+  }
+
+  const startSavingEvent  = async( calendarEvent ) => {
+    
+    try {
+      if ( calendarEvent.id ) {
+        // Actualizando
+         await calendarApi.put( `/events/${ calendarEvent.id }`, calendarEvent )
+        dispatch( onUpdateEvent({ ...calendarEvent, user }))
+        return
+      } 
+      // creando
+        const { data } = await calendarApi.post('/events', calendarEvent);
+        dispatch( onAddNewEvent({ ...calendarEvent, id:data.event.id, user }) )
+  
+    } catch (error) {
+      console.log(error);
+      Swal.fire('Error al guardar', error.response.data?.msg, 'error');
+    }
+
+  
+  }
+
+  const startDeleteEvent =  async( ) => {
+
+    try {
+      await calendarApi.delete(`/events/${ activeEvent.id }`)  
+      dispatch( onDeleteEvent() )
+    } catch (error) {
+      console.log(error);
+      Swal.fire('Error al eliminar', error.response.data.msg, 'error');
+    }
+  }
+  
+  const startLoadingEvents = async() => {
+    try {
+
+      const {  data }  = await calendarApi.get('/events');
+      const events =  convertEventsToDateEvents( data.event )
+      dispatch( onLoadEvents( events ) )
+
+    } catch (error) {
+      console.log('Error cargando eventos');
+      console.log(error);
+      
+    }
+  }
+
+
+    return{
+      //propiedades
+      activeEvent, 
+      events,
+      hasEventSelected: !!activeEvent,  
+
+      //metodos
+      startDeleteEvent,
+      setActiveEvent,
+      startSavingEvent, 
+      startLoadingEvents
+    }
+}
